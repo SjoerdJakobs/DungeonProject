4568b1152322182b7a208d054810a406d284fd66
SjoerdJakobs <18284@ma-web.nl>
2016-09-07 15:04:35 +0200

    Added Plane
    Added plane generation and a script that spawns chambers(planes) in a
    certain given area.

---
+++ Assets/Scripts/ChamberCreator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChamberCreator : MonoBehaviour {
+    [SerializeField]
+    private int xSizeLimit = 100;
+    [SerializeField]
+    private int ySizeLimit = 100;
+    [SerializeField]
+    private int chambers = 15;
+    [SerializeField]
+    private Material chamberMaterial;
+
+    // Use this for initialization
+    void Start () {
+        CreateRooms();
+	}
+	
+    void CreateRooms()
+    {
+        for (int i = 0; i < chambers; i++)
+        {
+            GameObject go = new GameObject("chamber");
+            go.AddComponent<GeneratePlane>();
+            Renderer renderer = go.GetComponent<Renderer>();
+            renderer.material = chamberMaterial;
+            go.AddComponent<BoxCollider2D>();
+            go.transform.position = new Vector3(Random.Range(1, xSizeLimit), Random.Range(1, ySizeLimit), 0);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireCube(Vector3.zero + new Vector3(xSizeLimit/2, ySizeLimit/2,0), new Vector3(xSizeLimit, ySizeLimit, 0));
+    }
+}
---
+++ Assets/Scripts/GeneratePlane.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class GeneratePlane : MonoBehaviour
+{
+
+    public int xSize, ySize;
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    public corners _corners;
+    
+
+    private void Awake()
+    {
+        _corners = new corners();
+        xSize = Random.Range(5, 15);
+        ySize = Random.Range(5, 15);
+        Generate();
+    }
+
+    private void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Plane";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+
+            }
+        }
+        _corners.downLeftCorner = vertices[0];
+        _corners.downRightCorner = vertices[xSize];
+        _corners.upperRightCorner = vertices[(xSize + 1) * (ySize + 1) - 1];
+        _corners.upperLeftCorner = vertices[(xSize + 1) * (ySize + 1) - (xSize + 1)];
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+        mesh.triangles = triangles;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(_corners.downLeftCorner + transform.position, 0.4f);
+        Gizmos.DrawSphere(_corners.upperRightCorner + transform.position, 0.4f);
+        Gizmos.DrawSphere(_corners.downRightCorner + transform.position, 0.4f);
+        Gizmos.DrawSphere(_corners.upperLeftCorner + transform.position, 0.4f);
+    }
+
+    public struct corners
+    {
+        public Vector3 downLeftCorner;
+        public Vector3 downRightCorner;
+        public Vector3 upperLeftCorner;
+        public Vector3 upperRightCorner;
+    }
+    
+}
